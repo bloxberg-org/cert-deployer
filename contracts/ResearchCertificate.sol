538f651e58aa936dd08d5fcd1b055621e3e49747
James <crossoveranx@gmail.com>
2020-08-20 14:44:07 +0200

    updated tokenURI function

--- contracts/ResearchCertificate.sol
+++ contracts/ResearchCertificate.sol
@@ -2,15 +2,16 @@ pragma solidity ^0.6.0;
 
 import "/home/james/bloxberg-certs/cert-deployer/contracts/ERC721Full.sol";
 import "/home/james/bloxberg-certs/cert-deployer/contracts/Counters.sol";
+import "/home/james/bloxberg-certs/cert-deployer/contracts/Ownable.sol";
 
-contract ResearchCertificate is ERC721Full {
+contract ResearchCertificate is ERC721Full, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     constructor() ERC721Full("DigitalLabs", "SARI") public {
     }
 
-    function createCertificate(address recipient, string memory tokenURI, string memory tokenHash) public returns (uint256) {
+    function createCertificate(address recipient, string memory tokenURI, string memory tokenHash) onlyOwner public returns (uint256) {
         _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
@@ -19,5 +20,12 @@ contract ResearchCertificate is ERC721Full {
         _setTokenHash(newItemId, tokenHash);
 
         return newItemId;
+    }
+
+    function updateTokenURI(string memory tokenURI) onlyOwner public returns (string memory) { 
+	uint256 currentTokenId = _tokenIds.current();
+	_setTokenURI(currentTokenId, tokenURI);
+	
+	return tokenURI;
     }
 }
